c81b159e647d458462d67bc2cf170da1d9f1eaee
Yousef Majdi <alshenyouef@gmail.com>
2020-02-05 07:12:49 +0200

    added js file

---
+++ javascrit.js
@@ -0,0 +1,10 @@
+document.write("<h5> hi </h5>" );
+var txt;
+var r = confirm("ARE YOU READY!");
+if (r == true) {
+  txt = "You pressed OK!";
+} else {
+  txt = "You pressed Cancel!";
+}
+alert("NOT YET :D");
+prompt("your name plz");
